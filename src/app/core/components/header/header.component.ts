20b4bcf95a1016e8c7c75f1e93f1bc414157d32d
sasaafanasienka <you@example.comafanasienka@yandex.by>
2022-04-26 17:23:02 +0300

    feat: footer and header without lang toggler

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  toggleBoardModal = (): void => {
+    console.log('toggleBoardModal');
+  };
 
+  toggleProfileModal = (): void => {
+    console.log('toggleProfileModal');
+  };
+
+  logout = (): void => {
+    console.log('logout');
+  };
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,22 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    PageNotFoundComponent,
-  ],
+  declarations: [HeaderComponent, FooterComponent, PageNotFoundComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatMenuModule,
   ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    PageNotFoundComponent,
-  ],
+  exports: [HeaderComponent, FooterComponent, PageNotFoundComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
