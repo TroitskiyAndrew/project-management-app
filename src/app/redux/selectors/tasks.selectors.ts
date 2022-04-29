9889cc1c72a9c1f46f777617a42dbe6d8eb71b5d
sasaafanasienka <you@example.comafanasienka@yandex.by>
2022-04-29 18:22:58 +0300

    fix: pr corrections

--- src/app/redux/selectors/tasks.selectors.ts
+++ src/app/redux/selectors/tasks.selectors.ts
@@ -1,9 +1,4 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { createFeatureSelector } from '@ngrx/store';
 import { TasksState } from '@redux/state.models';
 
-export const tasksSelector = createSelector(
-  createFeatureSelector<TasksState>('tasks'),
-  (value): TasksState => {
-    return value;
-  },
-);
+export const tasksSelector = createFeatureSelector<TasksState>('tasks')
