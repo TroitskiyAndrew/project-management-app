f0e957f69bfdc023ee6f280d39f3df06e6c5c011
sasaafanasienka <you@example.comafanasienka@yandex.by>
2022-05-02 11:50:41 +0300

    feat: add angular-notifier lib

--- src/app/redux/effects/current-user.effetcs.ts
+++ src/app/redux/effects/current-user.effetcs.ts
@@ -2,14 +2,23 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { successResponseAction, errorResponseAction } from '@redux/actions/api-respone.actions';
+import {
+  successResponseAction,
+  errorResponseAction,
+} from '@redux/actions/api-respone.actions';
 import { logoutUserAction } from '@redux/actions/current-user.actions';
+import { NotifierService } from 'angular-notifier';
 import { of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private authService: AuthService, private router: Router) { }
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router,
+    private notifier: NotifierService,
+  ) {}
 
   public logIn$ = createEffect(() =>
     this.actions$.pipe(
@@ -18,12 +27,14 @@ export class AuthEffects {
         return this.authService.logIn(action.loginInfo).pipe(
           map(() => {
             this.router.navigate(['']);
+            this.notifier.notify('success', 'Successfull logged in');
             return successResponseAction();
           }),
           catchError((error) => of(errorResponseAction(error))),
         );
       }),
-    ));
+    ),
+  );
 
   public craeateUser$ = createEffect(() =>
     this.actions$.pipe(
@@ -32,50 +43,66 @@ export class AuthEffects {
         return this.authService.createUser(action.newUser).pipe(
           map(() => {
             this.router.navigate(['']);
+            this.notifier.notify('success', 'Successfull created');
             return successResponseAction();
           }),
           catchError((error) => of(errorResponseAction(error))),
         );
       }),
-    ));
+    ),
+  );
 
   public editUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType('[User] edit'),
       switchMap((action: any) => {
         return this.authService.editUser(action.newParams).pipe(
-          map(() => successResponseAction()),
+          map(() => {
+            this.notifier.notify('success', 'Successfull edited');
+            return successResponseAction();
+          }),
           catchError((error) => of(errorResponseAction(error))),
         );
       }),
-    ));
+    ),
+  );
 
   public deleteUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType('[User] delete'),
       switchMap((action: any) => {
         return this.authService.deleteUser(action.password).pipe(
-          map(() => logoutUserAction()),
+          map(() => {
+            this.notifier.notify('success', 'Successfull deleted');
+            return logoutUserAction();
+          }),
           catchError((error) => of(errorResponseAction(error))),
         );
       }),
-    ));
+    ),
+  );
 
-  public logOut$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType('[User] logout'),
-      tap(() => {
-        this.router.navigate(['']);
-        this.authService.cleareCookie();
-      }),
-    ), { dispatch: false });
+  public logOut$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType('[User] logout'),
+        tap(() => {
+          this.router.navigate(['']);
+          this.notifier.notify('success', 'Successfull logged out');
+          this.authService.cleareCookie();
+        }),
+      ),
+    { dispatch: false },
+  );
 
-  public restoreUser$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType('[User] restore'),
-      map(() => {
-        this.authService.restoreUser();
-      }),
-    ), { dispatch: false });
-
+  public restoreUser$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType('[User] restore'),
+        map(() => {
+          this.authService.restoreUser();
+        }),
+      ),
+    { dispatch: false },
+  );
 }
