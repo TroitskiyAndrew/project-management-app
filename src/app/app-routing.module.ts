6d47bc0ea09370a65732cdb1eb52fd4a3533444a
ShikiLe <mariajane1897@gmail.com>
2022-05-04 16:24:35 +0300

    feat: add guard for board route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 import { MainPageComponent } from '@core/pages/main-page/main-page.component';
 import { PageNotFoundComponent } from '@core/pages/page-not-found/page-not-found.component';
 import { WorkspacePageComponent } from '@core/pages/workspace-page/workspace-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: MainPageComponent },
-  { path: 'board', component: WorkspacePageComponent },
+  {
+    path: 'board',
+    component: WorkspacePageComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
   },
   { path: '**', component: PageNotFoundComponent },
 ];
@@ -18,4 +23,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
