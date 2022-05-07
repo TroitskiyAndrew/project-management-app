b688c02f3cec5f960f17760b2f5816cf6ce52f3a
ShikiLe <mariajane1897@gmail.com>
2022-05-07 13:26:48 +0300

    feat: add translation to main page

--- src/assets/localization.ts
+++ src/assets/localization.ts
@@ -142,6 +142,70 @@ const localization = {
     },
     button: { en: 'Create', ru: 'Создать' },
   },
+  mainPage: {
+    title: { en: 'Tracker', ru: 'Трекер' },
+    description: {
+      en: 'Monitor all your tasks with Tracker, a service for project management, resource allocation, and collaboration.',
+      ru: 'Контролируйте все свои задачи с помощью Трекера, сервиса для управления проектами, распределения ресурсов и сотрудничества.',
+    },
+    button: { en: 'Try it', ru: 'Попробуйте' },
+    tabOneContent: {
+      en: 'Set up Agile development projects in Tracker: estimate timelines, plan sprints, monitor issue progress on a virtual board, and track their completion on dashboards.',
+      ru: 'Настройте гибкую разработку проектов в Трекере: рассчитывайте сроки, планируйте спринты, следите за прогрессом задач на виртуальной доске и отслеживайте их завершение на дашбордах.',
+    },
+    tabOneLabel: { en: 'Development', ru: 'Разработка' },
+    tabTwoContent: {
+      en: 'Set up document flows in Tracker. Collaborate on and approve documents on the same platform to formalize processes and eliminate routine tasks.',
+      ru: 'Настройте документооборот в Трекере. Совместная работа над документами и их утверждение на одной платформе формализирует процесс и устраняет рутинные задачи.',
+    },
+    tabTwoLabel: { en: 'Document flow', ru: 'Документооборот' },
+    tabThreeContent: {
+      en: 'Build dashboards to prepare materials for marketing campaigns. Make graphs and statistics to visualize and organize processes. Time tracking and comments make work with contractors more transparent.',
+      ru: 'Создавайте дашборды для подготовки материалов для маркетинговых кампаний. Делайте графики и статистику для визуализации и организации процессов. Учёт рабочего времени и комментарии делают работу с подрядчиками более прозрачной.',
+    },
+    tabThreeLabel: { en: 'Marketing', ru: 'Маркетинг' },
+    tabFourContent: {
+      en: 'Use Tracker for iterative work. Discuss layouts with co-workers right on the issue page and don’t lose any comments. For standard issues, create project templates and automate the approval process.',
+      ru: 'Используйте Трекер для последовательной работы. Обсуждайте макеты с коллегами прямо на странице и не теряйте комментарии. Для стандартных задач создайте шаблоны проектов и автоматизируйте процесс утверждения.',
+    },
+    tabFourLabel: { en: 'Design', ru: 'Дизайн' },
+    tabFiveContent: {
+      en: 'Build HR processes in Tracker: from hiring new employees to evaluating workloads and approving vacations. Monitor a department’s activities using visual dashboards, collect statistics, and optimize processes.',
+      ru: 'Выстраивайте HR-процессы в Трекере: от найма новых сотрудников до оценки нагрузки и согласования отпусков. Отслеживайте деятельность отдела с помощью дашбордов, собирайте статистику и оптимизируйте процессы.',
+    },
+    tabFiveLabel: { en: 'Human resources', ru: 'Управление персоналом' },
+    tabSixContent: {
+      en: 'Use Tracker for infrastructure tasks: equipment purchases, inventory management, bookkeeping, and more. Configure key project SLAs to monitor progress.',
+      ru: 'Используйте Трекер для инфраструктурных задач: покупка оборудования, управление инвентаризацией, бухгалтерия и многое другое. Настройте ключевые SLA (соглашение об уровне обслуживания) проекта для мониторинга прогресса.',
+    },
+    tabSixLabel: { en: 'Infrastructure', ru: 'Инфраструктура' },
+    gridOneContent: {
+      en: 'Tracker is a convenient solution for businesses of all sizes, with a universal approach right for any industry.',
+      ru: 'Трекер — удобное решение для бизнеса любого размера, с универсальным подходом для любой отрасли.',
+    },
+    gridOneLabel: {
+      en: 'Flexible work environment',
+      ru: 'Гибкая рабочая среда',
+    },
+    gridTwoContent: {
+      en: 'Tracker is designed to handle heavy workloads.',
+      ru: 'Трекер предназначен для работы с большими нагрузками.',
+    },
+    gridTwoLabel: { en: 'Fault tolerance', ru: 'Отказоустойчивость' },
+    gridThreeContent: {
+      en: 'Our project planning software has best features as well as features of the team management software.',
+      ru: 'Наше программное обеспечение для планирования проектов обладает лучшими функциями, а также функциями для управления командой.',
+    },
+    gridThreeLabel: {
+      en: 'Manage team and track progress',
+      ru: 'Управляйте командой и отслеживайте прогресс',
+    },
+    gridFourContent: {
+      en: 'To make project management even easier, we let our users prioritize tasks, indent and outdent, set dependencies in one click, durations and progress right on a chart online by dragging and dropping tasks and their attributes.',
+      ru: 'Чтобы сделать управление проектами еще проще, мы позволяем нашим пользователям приоритизировать задачи, устанавливать отступы, устанавливать зависимости одним щелчком мыши, продолжительность и прогресс прямо на онлайн-диаграмме, перетаскивая задачи и их атрибуты.',
+    },
+    gridFourLabel: { en: 'Automatic online project scheduling and drag and drop simplicity', ru: 'Автоматическое планирование онлайн-проектов и простота перетаскивания' },
+  },
 };
 
 function readLocalization(obj: any, lang: string) {
