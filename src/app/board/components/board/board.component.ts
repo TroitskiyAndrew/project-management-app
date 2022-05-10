2aef6fab8dfb9e3b339e785dea09c79912fa0a69
ShikiLe <mariajane1897@gmail.com>
2022-05-11 01:34:34 +0300

    feat: add lists to board

--- src/app/board/components/board/board.component.ts
+++ src/app/board/components/board/board.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PortalService } from '@core/services/portal.service';
 import { Store } from '@ngrx/store';
-import { getColumnsAction } from '@redux/actions/columns.actions';
 import { columnsSelector, currentBoardSelector } from '@redux/selectors/boards.selectors';
 import { AppState } from '@redux/state.models';
 import { NewListModalComponent } from '@shared/components/new-list-modal/new-list-modal.component';
@@ -13,18 +12,12 @@ import { Observable } from 'rxjs';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
   currentBoard$: Observable<BoardModel | null> = this.store$.select(currentBoardSelector);
 
   columns$: Observable<ColumnModel[]> = this.store$.select(columnsSelector);
 
   constructor(private store$: Store<AppState>, private portalService: PortalService) { }
-
-  ngOnInit(): void {
-    this.currentBoard$.subscribe(val => console.log(val));
-    this.store$.dispatch(getColumnsAction());
-    this.columns$.subscribe(val => console.log(val));
-  }
 
   openListModal(): void {
     this.portalService.openComponent(NewListModalComponent);
