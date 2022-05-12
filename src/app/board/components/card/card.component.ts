af93b24cfe169a1b10b2d86de5d19085fc63db2c
ShikiLe <mariajane1897@gmail.com>
2022-05-12 16:51:17 +0300

    feat: implement task card

--- src/app/board/components/card/card.component.ts
+++ src/app/board/components/card/card.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { deleteTaskAction } from '@redux/actions/tasks.actions';
+import { selectAllUsers } from '@redux/selectors/users.selectors';
+import { AppState } from '@redux/state.models';
+import { TaskModel } from '@shared/models/board.model';
+import { IUser } from '@shared/models/user.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input() task!: TaskModel;
 
-  constructor() { }
+  public taskCreator!: IUser[];
 
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
   }
 
+  ngOnInit(): void {
+    this.store.select(selectAllUsers).subscribe(users => {
+      this.taskCreator = users.filter(user => user._id === this.task.userId);
+    });
+  }
+
+  deleteTask(): void {
+    this.store.dispatch(deleteTaskAction({ id: this.task._id }));
+  }
 }
