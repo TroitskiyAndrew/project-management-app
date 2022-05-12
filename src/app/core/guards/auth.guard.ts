1310c4fab8616ea74cb7cfe98fdfc6ffd8178768
Andrew <TroitskiyA@yandex.ru>
2022-05-12 16:12:22 +0500

    feat: fix authGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,28 +1,32 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, CanLoad, Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 import { CookieService } from 'ngx-cookie-service';
+import { catchError, Observable, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanLoad, CanActivate {
 
-  constructor(private router: Router, private cookieService: CookieService) { }
+  constructor(private router: Router, private cookieService: CookieService, private authService: AuthService) { }
 
-  canLoad(): boolean {
+  canLoad(): Observable<boolean> {
     return this.isAuth();
   }
 
-  canActivate(): boolean {
+  canActivate(): Observable<boolean> {
     return this.isAuth();
   }
 
-  private isAuth(): boolean {
-    const token = this.cookieService.get('project-manager-token');
-    if (!token) {
-      this.router.navigate(['user', 'login']);
+  private isAuth(): Observable<boolean> {
+    if (!this.cookieService.get('project-manager-token')) {
+      return of(false);
     }
-    return Boolean(token);
+    return this.authService.getUsers().pipe(
+      switchMap(() => of(true)),
+      catchError(() => of(false)),
+    );
   }
 
 }
