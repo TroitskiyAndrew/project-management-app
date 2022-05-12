0ea107cbe8b0e9a5027cc4d2e7c279e9d918f343
ShikiLe <mariajane1897@gmail.com>
2022-05-12 22:46:04 +0300

    refactor: refactor new task modal logic

--- src/app/board/components/list/list.component.ts
+++ src/app/board/components/list/list.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ConfirmService } from '@core/services/confirm.service';
-import { PortalService } from '@core/services/portal.service';
 import { Store } from '@ngrx/store';
 import {
   deleteColumnAction,
   updateColumnAction,
 } from '@redux/actions/columns.actions';
-import { tasksByColumnSelector } from '@redux/selectors/boards.selectors';
+import { createTaskAction } from '@redux/actions/tasks.actions';
+import { currentBoardIdSelector, tasksByColumnSelector } from '@redux/selectors/boards.selectors';
+import { selectCurrentUser } from '@redux/selectors/users.selectors';
 import { AppState } from '@redux/state.models';
-import { ColumnModel, NewColumnModel, TaskModel } from '@shared/models/board.model';
-import { Observable } from 'rxjs';
+import { ColumnModel, NewColumnModel, NewTaskModel, TaskModel } from '@shared/models/board.model';
+import { IUser } from '@shared/models/user.model';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { TaskModalComponent } from '../task-modal/task-modal.component';
 
 @Component({
@@ -20,23 +23,60 @@ import { TaskModalComponent } from '../task-modal/task-modal.component';
 export class ListComponent implements OnInit{
   @Input() column!: ColumnModel;
 
+  private destroy$ = new Subject<void>();
+
+  private currentBoardId!: string;
+
+  private taskOrder!: number;
+
   public tasks$!: Observable<TaskModel[]>;
+
+  public currentUser!: IUser | null | undefined;
 
   isEditable: boolean = false;
 
   constructor(
-    private store: Store<AppState>,
+    private store$: Store<AppState>,
     private confirmService: ConfirmService,
-    private portalService: PortalService,
+    public dialog: MatDialog,
   ) {}
 
   ngOnInit(): void {
-    this.tasks$ = this.store.select(tasksByColumnSelector(this.column._id));
+    this.tasks$ = this.store$.select(tasksByColumnSelector(this.column._id));
+    this.store$
+      .select(currentBoardIdSelector)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((id) => {
+        if (id) {
+          this.currentBoardId = id;
+        }
+      });
+    this.store$
+      .select(tasksByColumnSelector(this.column._id))
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((tasks) => {
+        this.taskOrder = tasks.length;
+      });
+    this.store$.select(selectCurrentUser).subscribe((res) => (this.currentUser = res));
   }
 
   openTaskModal(): void {
-    this.portalService.openComponent(TaskModalComponent, {
-      columnId: this.column._id,
+    const dialogRef = this.dialog.open(TaskModalComponent, {
+      panelClass: 'dialog__panel',
+      hasBackdrop: true,
+      autoFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      const newTask: NewTaskModel = {
+        ...res,
+        boardId: this.currentBoardId,
+        order: this.taskOrder,
+        columnId: this.column._id,
+        userId: this.currentUser?._id,
+      };
+
+      this.store$.dispatch(createTaskAction({ newTask }));
     });
   }
 
@@ -46,14 +86,14 @@ export class ListComponent implements OnInit{
       order: this.column.order,
       title: value,
     };
-    this.store.dispatch(updateColumnAction({ newParams, id: this.column._id }));
+    this.store$.dispatch(updateColumnAction({ newParams, id: this.column._id }));
     this.isEditable = !this.isEditable;
   }
 
   deleteList() {
     this.confirmService.requestConfirm().subscribe((val) => {
       if (val) {
-        this.store.dispatch(deleteColumnAction({ id: this.column._id }));
+        this.store$.dispatch(deleteColumnAction({ id: this.column._id }));
       }
     });
   }
--- src/app/board/components/task-modal/task-modal.component.ts
+++ src/app/board/components/task-modal/task-modal.component.ts
@@ -1,63 +1,49 @@
 import {
   Component,
   ElementRef,
-  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { PortalService } from '@core/services/portal.service';
 import { Store } from '@ngrx/store';
-import {
-  currentBoardIdSelector,
-  tasksByColumnSelector,
-} from '@redux/selectors/boards.selectors';
 import { AppState } from '@redux/state.models';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import {
   selectAllUsers,
-  selectCurrentUser,
 } from '@redux/selectors/users.selectors';
 import { IUser } from '@shared/models/user.model';
-import { NewTaskModel } from '@shared/models/board.model';
-import { createTaskAction } from '@redux/actions/tasks.actions';
+
+interface NewTaskFormData {
+  title: string,
+  description: string,
+  users: string[],
+}
 
 @Component({
   selector: 'app-task-modal',
   templateUrl: './task-modal.component.html',
   styleUrls: ['./task-modal.component.scss'],
 })
-export class TaskModalComponent implements OnInit, OnDestroy {
+export class TaskModalComponent implements OnInit {
   @ViewChild('memberCtrl')
     memberCtrl!: ElementRef<HTMLInputElement>;
 
-  private destroy$ = new Subject<void>();
-
   public allUsers$: Observable<IUser[]> = this.store$.select(selectAllUsers);
-
-  private columnId: string = this.portalService.data!['columnId'] as string;
-
-  private currentUser$: Observable<IUser | null> = this.store$.select(selectCurrentUser);
 
   public createTaskForm!: FormGroup;
 
-  private currentBoardId!: string;
-
-  public currentUser!: IUser | null;
-
   public separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  public selectedUsers: string[] = [];
+  public selectedUsers: string[] = ['lena'];
 
-  private taskOrder!: number;
+  public formData!: NewTaskFormData;
 
   constructor(
     private store$: Store<AppState>,
     private formBuilder: FormBuilder,
-    private portalService: PortalService,
   ) {}
 
   ngOnInit(): void {
@@ -66,23 +52,6 @@ export class TaskModalComponent implements OnInit, OnDestroy {
       description: ['', [Validators.required]],
       users: [''],
     });
-    this.store$
-      .select(currentBoardIdSelector)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((id) => {
-        if (id) {
-          this.currentBoardId = id;
-        }
-      });
-
-    this.store$
-      .select(tasksByColumnSelector(this.columnId))
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((tasks) => {
-        this.taskOrder = tasks.length;
-      });
-
-    this.currentUser$.subscribe((res) => (this.currentUser = res));
   }
 
   add(event: MatChipInputEvent): void {
@@ -112,22 +81,10 @@ export class TaskModalComponent implements OnInit, OnDestroy {
     this.createTaskForm.controls['users'].setValue(null);
   }
 
-  onSubmit(): void {
-    const newTask: NewTaskModel = {
+  update() {
+    this.formData = {
       ...this.createTaskForm.value,
       users: this.selectedUsers,
-      boardId: this.currentBoardId,
-      order: this.taskOrder,
-      columnId: this.columnId,
-      userId: this.currentUser?._id,
     };
-
-    this.store$.dispatch(createTaskAction({ newTask }));
-    this.portalService.close();
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
