d24274a3d68fb73ecfbc56a936aa5766dc970092
Andrew <TroitskiyA@yandex.ru>
2022-05-14 11:23:09 +0500

    feat: create column modal

--- src/app/board/components/board/board.component.ts
+++ src/app/board/components/board/board.component.ts
@@ -1,55 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { columnsByCurrentBoardSelector, currentBoardSelector } from '@redux/selectors/boards.selectors';
+import { columnsByCurrentBoardSelector } from '@redux/selectors/boards.selectors';
 import { AppState } from '@redux/state.models';
 import { NewListModalComponent } from 'src/app/board/components/new-list-modal/new-list-modal.component';
-import { BoardModel, ColumnModel, NewColumnModel } from '@shared/models/board.model';
-import { Observable, Subject, takeUntil } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { createColumnAction } from '@redux/actions/columns.actions';
+import { ColumnModel } from '@shared/models/board.model';
+import { Observable } from 'rxjs';
+import { PortalService } from '@core/services/portal.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements OnInit {
-  private currentBoard$: Observable<BoardModel | null> = this.store$.select(currentBoardSelector);
-
-  private currentBoardId!: string | undefined;
-
-  private existedColumnsCount!: number;
-
-  private destroy$ = new Subject<void>();
+export class BoardComponent {
 
   public columns$: Observable<ColumnModel[]> = this.store$.select(columnsByCurrentBoardSelector);
 
-  constructor(private store$: Store<AppState>, public dialog: MatDialog) { }
+  constructor(private store$: Store<AppState>, public portalService: PortalService) { }
 
-  ngOnInit(): void {
-    this.currentBoard$.subscribe(res => this.currentBoardId = res?._id);
-    this.store$.select(columnsByCurrentBoardSelector).pipe(takeUntil(this.destroy$)).subscribe((columns) => {
-      this.existedColumnsCount = columns.length;
-    });
-  }
 
   openListModal(): void {
-    const dialogRef = this.dialog.open(NewListModalComponent, {
-      panelClass: 'dialog__panel',
-      hasBackdrop: true,
-      autoFocus: false,
-    });
-
-    dialogRef.afterClosed().subscribe(res => {
-      if (res) {
-        const newColumn: NewColumnModel = {
-          ...res,
-          boardId: this.currentBoardId,
-          order: this.existedColumnsCount + 1,
-        };
-        this.store$.dispatch(createColumnAction({ newColumn }),
-        );
-      }
-    });
+    this.portalService.openComponent(NewListModalComponent);
   }
 }
--- src/app/board/components/new-list-modal/new-list-modal.component.ts
+++ src/app/board/components/new-list-modal/new-list-modal.component.ts
@@ -1,22 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PortalService } from '@core/services/portal.service';
+import { Store } from '@ngrx/store';
+import { createColumnAction } from '@redux/actions/columns.actions';
+import { columnsByCurrentBoardSelector, currentBoardIdSelector } from '@redux/selectors/boards.selectors';
+import { AppState } from '@redux/state.models';
+import { NewColumnModel } from '@shared/models/board.model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-new-list-modal',
   templateUrl: './new-list-modal.component.html',
   styleUrls: ['./new-list-modal.component.scss'],
 })
-export class NewListModalComponent implements OnInit {
+export class NewListModalComponent implements OnInit, OnDestroy {
+
+  private destroy$ = new Subject<void>();
 
   public createListForm!: FormGroup;
 
+  private currentBoardId!: string;
+
+  private existedColumnsCount!: number;
+
   constructor(
+    private store$: Store<AppState>,
     private formBuilder: FormBuilder,
+    private portalService: PortalService,
   ) { }
 
   ngOnInit(): void {
     this.createListForm = this.formBuilder.group({
       title: ['', [Validators.required]],
     });
+    this.store$.select(currentBoardIdSelector).pipe(takeUntil(this.destroy$)).subscribe((id) => {
+      if (id) {
+        this.currentBoardId = id;
+      }
+    });
+    this.store$.select(columnsByCurrentBoardSelector).pipe(takeUntil(this.destroy$)).subscribe((columns) => {
+      this.existedColumnsCount = columns.length;
+    });
+  }
+
+  onSubmit(): void {
+    const newColumn: NewColumnModel = {
+      ...this.createListForm.value,
+      boardId: this.currentBoardId,
+      order: this.existedColumnsCount + 1,
+    };
+    this.store$.dispatch(createColumnAction({ newColumn }),
+    );
+    this.portalService.close();
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy');
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
